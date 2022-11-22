f50fb6bad2929bbea3aff53810f4947c8e2cdf1e
mihvlad08 <mihvlad08@gmail.com>
2022-11-22 03:16:10 +0200

    create class and sketches for future functions

--- helper.py
+++ helper.py
@@ -7,18 +7,27 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 action = ActionChains(driver)
 
-def login():
+class InstagramWinnerInterface:
+    def login(self):
 
-    driver.get("https://www.instagram.com/")
-    driver.maximize_window()
+        driver.get("https://www.instagram.com/")
+        driver.maximize_window()
 
-    accept_cookies = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')
-    accept_cookies.click()
+        accept_cookies = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')
+        accept_cookies.click()
 
-    login_input = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
-    login_input.send_keys(str(credentials.username))
+        login_input = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
+        login_input.send_keys(str(credentials.username))
 
-    password_input = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
-    password_input.send_keys(str(credentials.password))
+        password_input = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
+        password_input.send_keys(str(credentials.password))
 
-    password_input.send_keys(Keys.ENTER)
+        password_input.send_keys(Keys.ENTER)
+
+    def goToGiveawayPost(self):
+        return
+    def likePost(self):
+        return
+    def commentOnPost(self):
+        return
+
