45609b0f70ba652e0d9509edb89e9f77fde35bdc
mihvlad08 <mihvlad08@gmail.com>
2022-11-22 12:58:09 +0200

    fix login issue, add class constructor and waiting time for 2FA

--- helper.py
+++ helper.py
@@ -8,9 +8,14 @@ driver = webdriver.Firefox()
 action = ActionChains(driver)
 
 class InstagramWinnerInterface:
-    def login(self):
 
+    def __init__(self, giveawayURL, twoFA):
+        self.twoFA = twoFA
+        self.giveawayURL = giveawayURL
+
+    def login(self):
         driver.get("https://www.instagram.com/")
+        driver.add_cookie({"name": "csrftoken", "value": "3T5swQiwY5ndMhhb8qbuNLybdxvfmzGg"})
         driver.maximize_window()
 
         accept_cookies = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')
@@ -22,12 +27,18 @@ class InstagramWinnerInterface:
         password_input = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
         password_input.send_keys(str(credentials.password))
 
+        # This simulates submitting the form to a certain level
         password_input.send_keys(Keys.ENTER)
+
+        if self.twoFA:
+            driver.implicitly_wait(30)
 
     def goToGiveawayPost(self):
         return
+
     def likePost(self):
         return
+
     def commentOnPost(self):
         return
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import helper
+url = 'https://realpython.com/python-class-constructor/'
+twoFA = True  # or False if you don't have two-factor on
 
-interface = helper.InstagramWinnerInterface
+interface = helper.InstagramWinnerInterface(url, twoFA)
 interface.login()
